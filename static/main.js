232fdbbdd3bdf97784893ff24622108058cf59de
ar0ne <camtbitakou@yandex.ru>
2021-12-31 12:56:48 +0300

    split files

---
+++ static/main.js
@@ -0,0 +1,95 @@
+/**
+
+
+**/
+var STORAGE_KEY = "board-game-deals";
+// cleanup localstorage
+localStorage.setItem(STORAGE_KEY, "[]");
+
+var dealsStorage = {
+    fetch: function() {
+        var deals = JSON.parse(localStorage.getItem(STORAGE_KEY) || "[]");
+        deals.forEach(function(deal, index) {
+            deals.id = index;
+        });
+        dealsStorage.uid = deals.length;
+        return deals;
+    },
+    save: function(deals) {
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(deals));
+    }
+};
+
+
+var app = new Vue({
+    data: {
+        game: "",
+        deals: dealsStorage.fetch(),
+    },
+    watch: {
+        deals: {
+            handler: function(games) {
+                dealsStorage.save(games);
+            },
+            deep: true
+        }
+    },
+    computed: {
+        // filter by price
+        filteredDeals: function() {
+            var sorted_deals = [...this.deals];
+            sorted_deals.sort(function(a, b) {
+                if (!a.price) {
+                    return false;
+                }
+                if (!b.price) {
+                    return True;
+                }
+                return a.price.amount - b.price.amount
+            });
+            return sorted_deals;
+        },
+    },
+    methods: {
+        searchGame: function() {
+            this.cleanupGames();
+            var value = this.game && this.game.trim();
+            if (!value) {
+                return;
+            }
+            const evtSource = new EventSource("/api/v1/stream/games?game=" + value);
+            var that = this;
+            evtSource.addEventListener("update", function(event) {
+                console.log(event);
+                var new_deals = JSON.parse(event["data"]);
+                new_deals.forEach(deal => {
+                    that.deals.push({
+                        id: dealsStorage.uid++,
+                        subject: deal["subject"],
+                        description: deal["description"],
+                        images: deal["images"],
+                        location: deal["location"],
+                        owner: deal["owner"],
+                        price: deal["price"],
+                        source: deal["source"],
+                        url: deal["url"],
+                        price_converted: deal["price_converted"],
+                    });
+                });
+                this.game = "";
+            });
+            evtSource.addEventListener("end", function(event) {
+                console.log('Handling end....')
+                evtSource.close();
+            });
+
+            // todo: clean up previous results
+        },
+        cleanupGames: function() {
+            if (this.deals) {
+                this.deals.splice(0, this.deals.length);
+            }
+        }
+    }
+});
+app.$mount(".app-container");
