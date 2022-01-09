81a4f10cff94d48f39af717f285d730dc4954d3d
ar0ne <camtbitakou@yandex.ru>
2022-01-10 02:16:47 +0300

    refactoring

--- bgd/services/base.py
+++ bgd/services/base.py
@@ -76,52 +76,56 @@ class GameSearchService(ABC):
     async def do_search(self, query: str, *args, **kwargs) -> Tuple[GameSearchResult]:
         """search query"""
 
-    def filter_results(self, products: list, filter_func: Optional[Callable] = None) -> list:
+    def filter_results(
+        self, products: Sequence, filter_func: Optional[Callable] = None
+    ) -> Sequence:
         """Filter valid results"""
         if not products:
-            return []
+            return ()
         if not filter_func:
             return products
-        return list(filter(filter_func, products))
+        return tuple(filter(filter_func, products))
 
     @cache()
     async def search(self, query: str, *args, **kwargs) -> Sequence[dict]:
-        """Searching games"""
+        """Searches a game by query"""
         log.info("Search data by: %s", self._client.__class__.__name__)
         responses = await asyncio.gather(
             self.do_search(query, *args, **kwargs), return_exceptions=True
         )
-        self._log_errors(responses)
-        # filter exceptions
-        results = tuple(filter(lambda r: r and isinstance(r, collections.abc.Sequence), responses))
-        # extract results if results is not empty
-        search_results = results[0] if results else results
+        # filter out non errors
+        search_results = self.cleanup_responses(responses)
         # add prices in different currencies
-        search_results_priced = [
-            await self.prepare_prices(result) for result in search_results  # type: ignore
-        ]
+        search_results_priced = [await self.convert_price(result) for result in search_results]
         # convert from dto to dicts, to make possible to cache it
         return tuple(res.dict() for res in search_results_priced)
 
-    def build_results(self, items: Optional[list]) -> Tuple[GameSearchResult]:
+    def build_results(self, items: Optional[Sequence[dict]]) -> Tuple[GameSearchResult]:
         """prepare search results for end user"""
         if not items:
             return ()  # type: ignore
         return tuple(map(self.result_factory.create, items))  # type: ignore
 
-    def _log_errors(self, all_responses: Tuple[Union[Any, Exception]]) -> None:
-        """Log errors if any occur"""
+    def cleanup_responses(
+        self, all_responses: Tuple[Union[Any, Exception]]
+    ) -> Tuple[GameSearchResult]:
+        """Filter responses and log errors if any occur"""
+        cleared_responses = []
         for resp in all_responses:
-            if not isinstance(resp, tuple):
+            if isinstance(resp, collections.abc.Sequence):
+                cleared_responses.append(resp)
+            else:
                 log.warning(
                     "Error appeared during searching: %s in %s",
                     resp,
                     self._client.__class__.__name__,
                     exc_info=True,
                 )
+        # extract results if results is not empty
+        return cleared_responses[0] if cleared_responses else cleared_responses  # type: ignore
 
-    async def prepare_prices(self, result: GameSearchResult) -> GameSearchResult:
-        """add prices in different currencies"""
+    async def convert_price(self, result: GameSearchResult) -> GameSearchResult:
+        """Add price in different currencies"""
         if not result.prices:
             return result
         base_price = result.prices[0]
@@ -159,7 +163,7 @@ class SimpleSuggestGameService(SuggestGameService):
 
     def __init__(self, games: str) -> None:
         """Init service"""
-        self.games = [game.strip() for game in games.split(",")]
+        self.games = tuple(game.strip() for game in games.split(","))
 
     async def suggest(self) -> str:
         """Suggest random game from the list"""
